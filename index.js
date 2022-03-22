e84ff60f3b92348a515c63823556361f05958b53
Destyni <Destyninjohnson20@icloud.com>
2022-03-22 17:31:22 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+function saturdayFun(activity) {
+   if (activity === undefined) {
+    return `This Saturday, I want to roller-skate!`
+} else activity === String
+return `This Saturday, I want to bathe my dog!`
+}
+
+function mondayWork(task) {
+    if (task === undefined) {
+        return `This Monday, I will go to the office.`
+    } else task === String
+    return `This Monday, I will work from home.`
+}
+
+function wrapAdjective(programmer) {
+    return function (message) {
+        return `You are ${programmer}${message}${programmer}!`
+    }
+}
